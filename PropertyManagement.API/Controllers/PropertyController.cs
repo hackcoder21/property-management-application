d2a6ecbbda8fb8c3b4fc318b1f6affe0742ea6ef
Rajat Jadhav <Jadhavrajat1998@gmail.com>
2025-11-11 18:00:15 +0530

    Add UpdateProperty method and improve NotFound response

--- PropertyManagement.API/Controllers/PropertyController.cs
+++ PropertyManagement.API/Controllers/PropertyController.cs
@@ -67,7 +67,7 @@ namespace PropertyManagement.API.Controllers
 
             if (propertyDomain == null)
             {
-                return NotFound();
+                return NotFound("Property not found");
             }
 
             // Map domain model to Dto
@@ -155,6 +155,67 @@ namespace PropertyManagement.API.Controllers
             };
 
             return CreatedAtAction(nameof(GetPropertyById), new { id = propertyDto.Id }, propertyDto);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult UpdateProperty([FromRoute] Guid id, [FromBody] UpdatePropertyRequestDto updatePropertyRequestDto)
+        {
+            // Check if property exists
+            var propertyDomain = dbContext.Properties.FirstOrDefault(x => x.Id == id);
+
+            if (propertyDomain == null)
+            {
+                return NotFound("Property not found");
+            }
+
+            // Update domain model
+            propertyDomain.Title = updatePropertyRequestDto.Title;
+            propertyDomain.Price = updatePropertyRequestDto.Price;
+            propertyDomain.City = updatePropertyRequestDto.City;
+            propertyDomain.State = updatePropertyRequestDto.State;
+            propertyDomain.Locality = updatePropertyRequestDto.Locality;
+            propertyDomain.Pincode = updatePropertyRequestDto.Pincode;
+            propertyDomain.NoOfRooms = updatePropertyRequestDto.NoOfRooms;
+            propertyDomain.CarpetAreaSqft = updatePropertyRequestDto.CarpetAreaSqft;
+            propertyDomain.BuiltYear = updatePropertyRequestDto.BuiltYear;
+            propertyDomain.Balcony = updatePropertyRequestDto.Balcony;
+            propertyDomain.Parking = updatePropertyRequestDto.Parking;
+            propertyDomain.PropertyImageUrl = updatePropertyRequestDto.PropertyImageUrl;
+            propertyDomain.HallImageUrl = updatePropertyRequestDto.HallImageUrl;
+            propertyDomain.KitchenImageUrl = updatePropertyRequestDto.KitchenImageUrl;
+            propertyDomain.BathroomImageUrl = updatePropertyRequestDto.BathroomImageUrl;
+            propertyDomain.BedroomImageUrl = updatePropertyRequestDto.BedroomImageUrl;
+            propertyDomain.ParkingImageUrl = updatePropertyRequestDto.ParkingImageUrl;
+
+            dbContext.SaveChanges();
+
+            // Map domain model to Dto
+            var propertyDto = new PropertyDto
+            {
+                Id = propertyDomain.Id,
+                UserId = propertyDomain.UserId,
+                Title = propertyDomain.Title,
+                Price = propertyDomain.Price,
+                City = propertyDomain.City,
+                State = propertyDomain.State,
+                Locality = propertyDomain.Locality,
+                Pincode = propertyDomain.Pincode,
+                NoOfRooms = propertyDomain.NoOfRooms,
+                CarpetAreaSqft = propertyDomain.CarpetAreaSqft,
+                BuiltYear = propertyDomain.BuiltYear,
+                Balcony = propertyDomain.Balcony,
+                Parking = propertyDomain.Parking,
+                PropertyImageUrl = propertyDomain.PropertyImageUrl,
+                HallImageUrl = propertyDomain.HallImageUrl,
+                KitchenImageUrl = propertyDomain.KitchenImageUrl,
+                BathroomImageUrl = propertyDomain.BathroomImageUrl,
+                BedroomImageUrl = propertyDomain.BedroomImageUrl,
+                ParkingImageUrl = propertyDomain.ParkingImageUrl,
+                User = propertyDomain.User
+            };
+
+            return Ok(propertyDto);
         }
     }
 }
