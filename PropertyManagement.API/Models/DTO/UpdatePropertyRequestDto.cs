143c23f60e9ad5d0fb39da9986ae40e2fea7b5ce
Rajat Jadhav <Jadhavrajat1998@gmail.com>
2025-11-11 17:59:34 +0530

    Add UpdatePropertyRequestDto class for property updates

---
+++ PropertyManagement.API/Models/DTO/UpdatePropertyRequestDto.cs
@@ -0,0 +1,24 @@
+namespace PropertyManagement.API.Models.DTO
+{
+    public class UpdatePropertyRequestDto
+    {
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Locality { get; set; }
+        public string Pincode { get; set; }
+        public int NoOfRooms { get; set; }
+        public float CarpetAreaSqft { get; set; }
+        public int BuiltYear { get; set; }
+        public bool Balcony { get; set; }
+        public bool Parking { get; set; }
+        public string? PropertyImageUrl { get; set; }
+        public string? HallImageUrl { get; set; }
+        public string? KitchenImageUrl { get; set; }
+        public string? BathroomImageUrl { get; set; }
+        public string? BedroomImageUrl { get; set; }
+        public string? ParkingImageUrl { get; set; }
+    }
+}
