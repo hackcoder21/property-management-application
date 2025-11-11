83512de197ca83463573e62f2eaf12876e390cef
Rajat Jadhav <Jadhavrajat1998@gmail.com>
2025-11-11 14:47:11 +0530

    Configure DbContext with SQL Server connection
    Added DbContext configuration for SQL Server.

--- PropertyManagement.API/Program.cs
+++ PropertyManagement.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PropertyManagement.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext class
+builder.Services.AddDbContext<PMDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PMConnectionString"));
+});
 
 var app = builder.Build();
 
