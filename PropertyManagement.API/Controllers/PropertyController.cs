65d5baad06c8caa7b97567b43912e865b87f33d9
Rajat Jadhav <Jadhavrajat1998@gmail.com>
2025-11-11 17:24:08 +0530

    Add CreateProperty method to PropertyController

--- PropertyManagement.API/Controllers/PropertyController.cs
+++ PropertyManagement.API/Controllers/PropertyController.cs
@@ -98,5 +98,63 @@ namespace PropertyManagement.API.Controllers
             // Return Dto response
             return Ok(propertyDto);
         }
+
+        [HttpPost]
+        public IActionResult CreateProperty([FromBody] AddPropertyRequestDto addPropertyRequestDto)
+        {
+            // Map Dto to domain model
+            var propertyDomain = new Property
+            {
+                UserId = addPropertyRequestDto.UserId,
+                Title = addPropertyRequestDto.Title,
+                Price = addPropertyRequestDto.Price,
+                City = addPropertyRequestDto.City,
+                State = addPropertyRequestDto.State,
+                Locality = addPropertyRequestDto.Locality,
+                Pincode = addPropertyRequestDto.Pincode,
+                NoOfRooms = addPropertyRequestDto.NoOfRooms,
+                CarpetAreaSqft = addPropertyRequestDto.CarpetAreaSqft,
+                BuiltYear = addPropertyRequestDto.BuiltYear,
+                Balcony = addPropertyRequestDto.Balcony,
+                Parking = addPropertyRequestDto.Parking,
+                PropertyImageUrl = addPropertyRequestDto.PropertyImageUrl,
+                HallImageUrl = addPropertyRequestDto.HallImageUrl,
+                KitchenImageUrl = addPropertyRequestDto.KitchenImageUrl,
+                BathroomImageUrl = addPropertyRequestDto.BathroomImageUrl,
+                BedroomImageUrl = addPropertyRequestDto.BedroomImageUrl,
+                ParkingImageUrl = addPropertyRequestDto.ParkingImageUrl
+            };
+
+            // Create property and save to database
+            dbContext.Properties.Add(propertyDomain);
+            dbContext.SaveChanges();
+
+            // Map domain model to Dto
+            var propertyDto = new PropertyDto
+            {
+                Id = propertyDomain.Id,
+                UserId = propertyDomain.UserId,
+                User = propertyDomain.User,
+                Title = propertyDomain.Title,
+                Price = propertyDomain.Price,
+                City = propertyDomain.City,
+                State = propertyDomain.State,
+                Locality = propertyDomain.Locality,
+                Pincode = propertyDomain.Pincode,
+                NoOfRooms = propertyDomain.NoOfRooms,
+                CarpetAreaSqft = propertyDomain.CarpetAreaSqft,
+                BuiltYear = propertyDomain.BuiltYear,
+                Balcony = propertyDomain.Balcony,
+                Parking = propertyDomain.Parking,
+                PropertyImageUrl = propertyDomain.PropertyImageUrl,
+                HallImageUrl = propertyDomain.HallImageUrl,
+                KitchenImageUrl = propertyDomain.KitchenImageUrl,
+                BathroomImageUrl = propertyDomain.BathroomImageUrl,
+                BedroomImageUrl = propertyDomain.BedroomImageUrl,
+                ParkingImageUrl = propertyDomain.ParkingImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetPropertyById), new { id = propertyDto.Id }, propertyDto);
+        }
     }
 }
