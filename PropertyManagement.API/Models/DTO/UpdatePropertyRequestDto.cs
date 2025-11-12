fe3b12cb266939972a8f29e12365badcf2743c1e
Rajat Jadhav <Jadhavrajat1998@gmail.com>
2025-11-12 12:36:13 +0530

    Remove UserId property from UpdatePropertyRequestDto

--- PropertyManagement.API/Models/DTO/UpdatePropertyRequestDto.cs
+++ PropertyManagement.API/Models/DTO/UpdatePropertyRequestDto.cs
@@ -2,7 +2,6 @@ namespace PropertyManagement.API.Models.DTO
 {
     public class UpdatePropertyRequestDto
     {
-        public Guid UserId { get; set; }
         public string Title { get; set; }
         public decimal Price { get; set; }
         public string City { get; set; }
