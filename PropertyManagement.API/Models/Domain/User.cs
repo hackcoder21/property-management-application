6674fb8aa9f6024854cf14c1b51620f41534dcc8
Rajat Jadhav <Jadhavrajat1998@gmail.com>
2025-11-14 11:59:29 +0530

    Change Property to Properties in User model

--- PropertyManagement.API/Models/Domain/User.cs
+++ PropertyManagement.API/Models/Domain/User.cs
@@ -5,6 +5,6 @@ namespace PropertyManagement.API.Models.Domain
         public Guid Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-        public Property Property { get; set; }
+        public List<Property> Properties { get; set; } = new List<Property>();
     }
 }
