6d2b907f7f838956c0f6c49523b6e1f9d53b3136
Rajat <jadhavrajat1998@gmail.com>
2025-12-31 13:41:30 +0530

    Created Anuglar project structure

--- PropertyManagement.UI/src/app/app.module.ts
+++ PropertyManagement.UI/src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserFormComponent } from './user/user-form/user-form.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { PropertyFormComponent } from './property/property-form/property-form.component';
+import { PropertyDetailsComponent } from './property/property-details/property-details.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PaginationComponent } from './shared/pagination/pagination.component';
+import { LoaderComponent } from './shared/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    UserFormComponent,
+    UserDetailsComponent,
+    PropertyListComponent,
+    PropertyFormComponent,
+    PropertyDetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+    PaginationComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
