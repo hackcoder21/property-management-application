6098d55a5fc67f4a6f53a29c3659f4b03bd803f8
Rajat <jadhavrajat1998@gmail.com>
2026-01-05 00:49:59 +0530

    Added property-details page

--- PropertyManagement.UI/src/app/property/property-details/property-details.component.ts
+++ PropertyManagement.UI/src/app/property/property-details/property-details.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Property } from 'src/app/core/models/property.model';
+import { PropertyService } from 'src/app/core/services/property.service';
 
 @Component({
   selector: 'app-property-details',
   templateUrl: './property-details.component.html',
-  styleUrls: ['./property-details.component.css']
+  styleUrls: ['./property-details.component.css'],
 })
-export class PropertyDetailsComponent {
+export class PropertyDetailsComponent implements OnInit {
+  userId!: string;
+  propertyId!: string;
+  isLoading: boolean = false;
 
+  property!: Property;
+
+  constructor(
+    private propertyService: PropertyService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.userId = this.route.snapshot.paramMap.get('userId')!;
+    this.propertyId = this.route.snapshot.paramMap.get('propertyId')!;
+    this.loadProperty();
+  }
+
+  loadProperty() {
+    this.isLoading = true;
+
+    this.propertyService.getPropertyByPropertyId(this.propertyId).subscribe({
+      next: (data) => {
+        this.property = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        alert('Error loading property');
+      },
+    });
+  }
+
+  onEditProperty() {
+    this.router.navigate([
+      `/dashboard/users/${this.userId}/properties/${this.propertyId}/edit`,
+    ]);
+  }
 }
